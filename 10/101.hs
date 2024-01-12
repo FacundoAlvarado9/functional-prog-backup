c503d39bf195375469e3b619fc292b3bd4406f98
Facundo Agustin Alvarado <facundo.alvarado@st.ovgu.de>
2024-01-12 22:57:35 +0100

    first from ü10

---
+++ 10/101.hs
@@ -0,0 +1,26 @@
+newtype EitherTransformer m a = EitherTransformer { runEitherTransformer :: m (Either String a) }
+
+instance Monad m => Applicative (EitherTransformer m) where
+    pure a = EitherTransformer $ return (Right a)
+    --(<*>) :: EitherTransformer m (a->b) -> EitherTransformer m a -> EitherTransformer m b
+    EitherTransformer f <*> EitherTransformer v = EitherTransformer $ do
+        mf <- f
+        case mf of
+            Left e -> return (Left e)
+            Right k -> do
+                mv <- v
+                case mv of
+                    Left e -> return (Left e)
+                    Right x -> return (Right (k x))
+
+instance Monad m => Functor (EitherTransformer m) where
+    fmap f = EitherTransformer . fmap (fmap f) . runEitherTransformer
+
+instance Monad m => Monad (EitherTransformer m) where
+    return = EitherTransformer . return . return
+    -- (>>=) :: EitherTransformer m a -> (a -> EitherTranformer m b) -> EitherTransoformer m b
+    x >>= f = EitherTransformer $ do 
+        either_value <- runEitherTransformer x -- I should get (Either String a)
+        case either_value of
+            Left msg   -> return (Left msg)
+            Right a    -> runEitherTransformer (f a)
